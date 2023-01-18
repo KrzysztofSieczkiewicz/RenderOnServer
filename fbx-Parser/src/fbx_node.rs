61e2b0997618299b0742e2ae0bcc48c693dea074
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-01-18 18:52:46 +0100

    added reader offset stored inside struct

--- fbx-Parser/src/fbx_node.rs
+++ fbx-Parser/src/fbx_node.rs
@@ -9,13 +9,24 @@ pub struct FbxNode<'a> {
 impl<'a> FbxNode<'a> {
     pub fn new(reader: &'a mut FbxReader<File>) -> FbxNode<'a> {
         FbxNode {
-            reader: reader
+            reader
         }
     }
 
     pub fn read_node(&mut self) {
 
         let end_offset = (self.reader).read_i32();
+        let num_properties = (self.reader).read_i32();
+        let property_list_length = (self.reader).read_i32();
+        let name_length = (self.reader).read_i8();
+
+        let bytes = 13 + name_length;
+        
+        println!("end_offset: {}", &end_offset);
+        println!("num_properties: {}", &num_properties);
+        println!("property_list_length: {}", &property_list_length);
+        println!("name_length: {}", &name_length);
+        println!("bytes: {}", &bytes);
     }
     
 }
--- fbx-Parser/src/fbx_reader.rs
+++ fbx-Parser/src/fbx_reader.rs
@@ -1,7 +1,9 @@
 use std::io::Read;
 pub struct FbxReader<R: Read> {
     reader: R,
+    pub offset: i32,
     buf_magic: [u8; 23],
+    buf_i8: [u8; 1],
     buf_i32: [u8; 4],
 }
 
@@ -9,7 +11,9 @@ impl<R: Read> FbxReader<R> {
     pub fn new(inner: R) -> FbxReader<R> {
         FbxReader {
             reader: inner,
+            offset: 0,
             buf_magic: [0; 23],
+            buf_i8: [0; 1],
             buf_i32: [0; 4],
         }
     }
@@ -18,6 +22,8 @@ impl<R: Read> FbxReader<R> {
         self.reader
             .read_exact(&mut self.buf_magic)
             .expect("Magic should be readable");
+        
+        self.offset += 23;
         return &self.buf_magic;
     }
 
@@ -25,29 +31,25 @@ impl<R: Read> FbxReader<R> {
         self.reader
             .read_exact(&mut self.buf_i32)
             .expect("Version should be readable");
+        
+        self.offset += 4;
         return &self.buf_i32;
     }
 
-    pub fn read_i32(&mut self) -> std::io::Result<i32> {
-        self.reader.read_exact(&mut self.buf_i32)?;
-        Ok(i32::from_le_bytes(self.buf_i32))
+    pub fn read_i8(&mut self) -> i8 {
+        self.reader.read_exact(&mut self.buf_i8)
+            .expect("i8 value should be readable");
+
+        self.offset += 1;
+        return i8::from_le_bytes(self.buf_i8)
     }
 
-    pub fn read_i64(&mut self) -> std::io::Result<i64> {
-        let mut buf = [0; 8];
-        self.reader.read_exact(&mut buf)?;
-        Ok(i64::from_le_bytes(buf))
+    pub fn read_i32(&mut self) -> i32 {
+        self.reader.read_exact(&mut self.buf_i32)
+            .expect("i32 value should be readable");
+
+        self.offset += 8;
+        return i32::from_le_bytes(self.buf_i32)
     }
 
-    pub fn read_f32(&mut self) -> std::io::Result<f32> {
-        let mut buf = [0; 4];
-        self.reader.read_exact(&mut buf)?;
-        Ok(f32::from_le_bytes(buf))
-    }
-
-    pub fn read_f64(&mut self) -> std::io::Result<f64> {
-        let mut buf = [0; 8];
-        self.reader.read_exact(&mut buf)?;
-        Ok(f64::from_le_bytes(buf))
-    }
 }
