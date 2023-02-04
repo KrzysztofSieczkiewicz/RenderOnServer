4017fcbccfad4dea7adc147f65aa5914f6596f28
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2023-02-04 16:42:41 +0100

    folder reorganization

--- fbx-Parser/target/debug/deps/fbx_Parser-1cd57a4999a8fbab.d
+++
@@ -1,9 +0,0 @@
-c:\Krzysiek\Programming\Back-end\RenderOnServer\fbx-Parser\target\debug\deps\fbx_Parser-1cd57a4999a8fbab.rmeta: src\main.rs src\fbx_property.rs src\fbx_file.rs src\fbx_reader.rs src\fbx_node.rs
-
-c:\Krzysiek\Programming\Back-end\RenderOnServer\fbx-Parser\target\debug\deps\fbx_Parser-1cd57a4999a8fbab.d: src\main.rs src\fbx_property.rs src\fbx_file.rs src\fbx_reader.rs src\fbx_node.rs
-
-src\main.rs:
-src\fbx_property.rs:
-src\fbx_file.rs:
-src\fbx_reader.rs:
-src\fbx_node.rs:
--- fbx-Parser/target/debug/deps/fbx_Parser-add18badd11726f2.d
+++
@@ -1,9 +0,0 @@
-c:\Krzysiek\Programming\Back-end\RenderOnServer\fbx-Parser\target\debug\deps\fbx_Parser-add18badd11726f2.rmeta: src\main.rs src\fbx_property.rs src\fbx_file.rs src\fbx_reader.rs src\fbx_node.rs
-
-c:\Krzysiek\Programming\Back-end\RenderOnServer\fbx-Parser\target\debug\deps\fbx_Parser-add18badd11726f2.d: src\main.rs src\fbx_property.rs src\fbx_file.rs src\fbx_reader.rs src\fbx_node.rs
-
-src\main.rs:
-src\fbx_property.rs:
-src\fbx_file.rs:
-src\fbx_reader.rs:
-src\fbx_node.rs:
